b87f51473f3eaa49dad5171adf32546520109ae4
Jaqui-Morales2020 <61250103+Jaqui-Morales2020@users.noreply.github.com>
2020-02-19 13:40:03 -0600

    Add files via upload
    Segundo Ejercicio

---
+++ Area_triangulo.py
@@ -0,0 +1,8 @@
+print ("°°°°°_Área de un triángulo_°°°°°°")
+
+print ("°°°°°_By: Jackye Morales_°°°°°°")
+
+base = input("¿Cuál es la base?: ")
+altura=input("¿Cuál es la altura?: ")
+area=(int (base) * int (altura)) / 2
+print ("El resultado del triángulo es: " + str (area))
