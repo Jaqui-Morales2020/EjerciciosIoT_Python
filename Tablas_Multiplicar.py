a5d6d3ce92d52b22bc283cb20e785cd3cbff2514
Jaqui-Morales2020 <61250103+Jaqui-Morales2020@users.noreply.github.com>
2020-02-20 00:17:55 -0600

    Add files via upload
    ejercicio 4

---
+++ Tablas_Multiplicar.py
@@ -0,0 +1,33 @@
+def imprimir_tabla(numero):
+    # las tablas llegan hasta el 10
+    LIMITE = 10
+    # Comenzar en 1
+    contador = 1
+    while contador <= LIMITE:
+        resultado = contador * numero
+        print("{} x {} = {}".format(numero, contador, resultado))
+        # Incrementar contador para no caer en ciclo infinito
+        contador = contador + 1
+
+# Probar función
+print("-----------")
+imprimir_tabla(1)
+print("-----------")
+imprimir_tabla(2)
+print("-----------")
+imprimir_tabla(3)
+print("-----------")
+imprimir_tabla(4)
+print("-----------")
+imprimir_tabla(5)
+print("-----------")
+imprimir_tabla(6)
+print("-----------")
+imprimir_tabla(7)
+print("-----------")
+imprimir_tabla(8)
+print("-----------")
+imprimir_tabla(9)
+print("-----------")
+imprimir_tabla(10)
+print("-----------")
